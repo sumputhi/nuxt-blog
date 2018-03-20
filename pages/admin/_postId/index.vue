8be597b9533561ddbae65130a809509256c89c65
puthi <puthi_sum@kredit.com.kh>
2018-03-20 17:04:14 +0700

    reupload files

---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="admin-post-page">
+    <section class="update-form">
+      <h1 class="post-title">{{ loadedPost.title }}</h1>
+      <AdminPostForm :post="loadedPost" @submit="onSubmitted" />
+    </section>
+  </div>
+</template>
+
+<script>
+import AdminPostForm from '@/components/Admin/AdminPostForm'
+import axios from 'axios'
+
+export default {
+  layout: 'admin',
+  components: {
+    AdminPostForm
+  },
+  asyncData(context) {
+    return axios.get('https://nuxt-blog-puthi.firebaseio.com/posts/' + context.params.postId + '.json')
+      .then(res => {
+        return {
+          loadedPost: { ...res.data, id: context.params.postId }
+        }
+      })
+      .catch(e => context.error(e))
+  },
+  methods: {
+    onSubmitted(editedPost) {
+      this.$store.dispatch("editPost", editedPost)
+        .then(() => {
+          this.$router.push("/admin")
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
